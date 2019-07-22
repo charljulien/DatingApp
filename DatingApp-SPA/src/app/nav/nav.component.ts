a5febe980764b5b88285b2dcf2f36cc2267f93ec
JULIEN Charles <charles.julien@soprasteria.com>
2019-07-22 16:25:53 +0200

    Login client done

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,13 +10,26 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login() {
-      console.log(this.model);
+      this.authService.login(this.model).subscribe(next => {
+        console.log('Logged in successfully');
+      }, error => {
+        console.log('Failed to login');
+      });
   }
 
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
 }
