7d8dd124f7520d4d23366c11b4757f6fc13f008b
JULIEN Charles <charles.julien@soprasteria.com>
2019-08-05 15:38:58 +0200

    end of session 4

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -6,19 +6,22 @@ import {map} from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthService {
-baseUrl = 'http://localhost:5000/api/auth/'
+  baseUrl = 'http://localhost:5000/api/auth/'
 
-constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-login(model: any) {
-  return this.http.post(this.baseUrl + 'login', model).pipe(
-    map((response: any) => {
-      const user = response;
-      if (user) {
-        localStorage.setItem('token', user.token);
-      }
-    })
-  )
-}
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'login', model).pipe(
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('token', user.token);
+        }
+      })
+    );
+  }
 
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model);
+  }
 }
--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -20,6 +20,7 @@ namespace DatingApp.API.Controllers
             this._context = context;
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
