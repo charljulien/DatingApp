2e2f01985f47460e3a23f739f8b82aa61e0956c1
JULIEN Charles <charles.julien@soprasteria.com>
2019-08-22 13:43:12 +0200

    end of section 8

--- DatingApp.API/Dtos/PhotosForDetailedDto.cs
+++ DatingApp.API/Dtos/PhotosForDetailedDto.cs
@@ -1,15 +1,13 @@
 using System;
 
-namespace DatingApp.API.Models
+namespace DatingApp.API.Dtos
 {
-    public class Photo
+    public class PhotosForDetailedDto
     {
         public int Id { get; set; }
         public string Url { get; set; }
         public string Description { get; set; }
         public DateTime DateAdded { get; set; }
         public bool IsMain { get; set; }
-        public User User { get; set; }
-        public int UserId { get; set; }
     }
 }
