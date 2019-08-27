c05d1218b67e4f911da28da71430492ffde6586c
JULIEN Charles <charles.julien@soprasteria.com>
2019-08-27 09:15:05 +0200

    login pb fixed

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../_services/user.service';
-import { AlertifyService } from '../_services/alertify.service';
-import { User } from '../_modules/user';
+import { UserService } from '../../_services/user.service';
+import { AlertifyService } from '../../_services/alertify.service';
+import { User } from '../../_modules/user';
 
 @Component({
   selector: 'app-member-list',
